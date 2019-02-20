e087f13778ee3dd58786d85df76fbe3a63173157
Unknown <kisungqueso@hotmail.com>
2019-02-19 21:04:07 -0600

    Path router, bootstrap css reference added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AdminComponent } from './admin/admin.component';
+import { SubsidiaryComponent } from './subsidiary/subsidiary.component';
+import { GeneralViewComponent } from './general-view/general-view.component';
+
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'sucursal', component: SubsidiaryComponent },
+  { path: 'home', component: GeneralViewComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { MedicinesComponent } from './medicines/medicines.component';
+import { RolesComponent } from './roles/roles.component';
+import { LocationsComponent } from './locations/locations.component';
+import { AdminComponent } from './admin/admin.component';
+import { SubsidiaryComponent } from './subsidiary/subsidiary.component';
+import { GeneralViewComponent } from './general-view/general-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent,
+    DoctorsComponent,
+    MedicinesComponent,
+    RolesComponent,
+    LocationsComponent,
+    AdminComponent,
+    SubsidiaryComponent,
+    GeneralViewComponent
   ],
   imports: [
     BrowserModule,
